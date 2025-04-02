cf1021c7392a513d87a562c3676f9ee35b529c04
Samyrai47 <sivolobov.ilya2@gmail.com>
2025-04-02 22:12:09 +0300

    git init

---
+++ src/main/java/mipt/projectCassandra/ProjectCassandraApplication.java
@@ -0,0 +1,11 @@
+package mipt.projectCassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCassandraApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectCassandraApplication.class, args);
+  }
+}
